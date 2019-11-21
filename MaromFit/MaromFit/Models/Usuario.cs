66dc9c91ebd884cffdf2ea018745dcfa2579f6d2
AndersonHavro <andersonhavro@gmail.com>
2019-11-21 15:11:49 -0300

    mvc inicio

---
+++ MaromFit/MaromFit/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaromFit.Models
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
